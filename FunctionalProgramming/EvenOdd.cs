7e67417af8bf9533e0836aadfa73ae88577d12a1
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-28 12:09:52 +0530

    [EvenOdd file Added]

---
+++ FunctionalProgramming/EvenOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class EvenOdd
+    {
+        public static void EvenAndOdd()
+        {
+            Console.WriteLine("Enter a number:");
+            Random rnd = new Random();
+            int num = rnd.Next(35);
+            Console.WriteLine("thw num is " + num);
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " The number is even.");
+            }
+            else
+            {
+                Console.WriteLine(num + " The number is odd.");
+            }
+        }
+    }
+}
