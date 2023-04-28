ae1678c037f1136fbddac9315cafd8fc2f56fc60
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-28 11:42:11 +0530

    [Added :- Program.cs file]

---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             VowelConsonant.checkedAlphabet();
+            
+            Console.ReadKey();
+           
+        }
+    }
+}
