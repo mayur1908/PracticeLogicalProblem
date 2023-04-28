be56b7e079e9d13e4514383dc043800399472ef3
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-28 12:06:34 +0530

    [QuotionatAndReminder file Added]

---
+++ FunctionalProgramming/QuotionatAndReminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class QuotionatAndReminder
+    {
+        public static void QuotionatReminder()
+        {
+            Console.WriteLine("WelllCome To QuotionatAndReminder");
+            Console.WriteLine("Enter the Dividend");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            int divisor= Convert.ToInt32(Console.ReadLine());
+            int quo = divident / divisor;
+            int rem = divident % divisor;
+            Console.WriteLine("the quotiont is  " + quo);
+            Console.WriteLine("the remider is  "+ rem);
+        }
+        
+    }
+}
